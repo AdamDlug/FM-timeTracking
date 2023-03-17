5dccb0791ddfac60e12cf317ff2159a11ff20c23
AdamDlug <89133957+AdamDlug@users.noreply.github.com>
2023-03-17 09:47:11 +0100

    Update script.js

--- time-tracking-dashboard-main/script.js
+++ time-tracking-dashboard-main/script.js
@@ -1,9 +1,9 @@
 const DailyInformations = document.getElementById('DailyInformations')
 const WeeklyInformations = document.getElementById('WeeklyInformations')
 const MonthlyInformations = document.getElementById('MonthlyInformations')
-const test1 = document.getElementsByName('daily')
-const test2 = document.getElementsByName('weekly')
-const test3 = document.getElementsByName('monthly')
+const dailyCards = document.getElementsByName('daily')
+const weeklyCards = document.getElementsByName('weekly')
+const monthlyCards = document.getElementsByName('monthly')
 
 const cardChange = (e, f, g) => {
     for (let i = 0; i < e.length; i++) {
@@ -17,13 +17,13 @@ const cardChange = (e, f, g) => {
     }
 }
 DailyInformations.addEventListener('click', function() {
-    cardChange(test1, test2, test3)
+    cardChange(dailyCards, weeklyCards, monthlyCards)
 })
 WeeklyInformations.addEventListener('click', function() {
-    cardChange(test2, test1, test3)
+    cardChange(weeklyCards, dailyCards, monthlyCards)
 })
 MonthlyInformations.addEventListener('click', function() {
-    cardChange(test3, test1, test2)
+    cardChange(monthlyCards, dailyCards, weeklyCards)
 })
 const testMouseOver = document.getElementsByClassName('dots')
 for (let i = 0; i < testMouseOver.length; i++) {
