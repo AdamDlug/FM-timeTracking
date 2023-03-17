ec63f7792fdf05e13ffc2ba45773d3b2d4317608
AdamDlug <89133957+AdamDlug@users.noreply.github.com>
2023-03-17 09:45:48 +0100

    Update script.js

--- time-tracking-dashboard-main/script.js
+++ time-tracking-dashboard-main/script.js
@@ -5,7 +5,6 @@ const test1 = document.getElementsByName('daily')
 const test2 = document.getElementsByName('weekly')
 const test3 = document.getElementsByName('monthly')
 
-
 const cardChange = (e, f, g) => {
     for (let i = 0; i < e.length; i++) {
         e[i].classList.remove('hidden')
@@ -17,30 +16,22 @@ const cardChange = (e, f, g) => {
         g[i].classList.add('hidden')
     }
 }
-
-
 DailyInformations.addEventListener('click', function() {
     cardChange(test1, test2, test3)
 })
-
 WeeklyInformations.addEventListener('click', function() {
     cardChange(test2, test1, test3)
 })
 MonthlyInformations.addEventListener('click', function() {
     cardChange(test3, test1, test2)
 })
-
-
-/////////////////////////////////////////////////////////////////////
-/////////////////  po najechaniu na element myszką zmienia tło
 const testMouseOver = document.getElementsByClassName('dots')
 for (let i = 0; i < testMouseOver.length; i++) {
     testMouseOver[i].onmouseover = e => {
-        e.stopPropagation()   // usuwa hierarchie - focus tylko na tym elemencie
+        e.stopPropagation()  
         testMouseOver[i].style.backgroundColor = "hsl(235, 46%, 20%)"
     }
     testMouseOver[i].onmouseout = e => {
         testMouseOver[i].style.backgroundColor = "hsl(235, 46%, 20%)"
     }
 }
-
